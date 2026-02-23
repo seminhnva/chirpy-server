9cf8f084595f66b635b7d157c835b5736bff7d4f
minhnva <fe.minhnva@gmail.om>
2026-02-23 15:44:20 +0700

    implement validate chirp

--- helper.go
+++ helper.go
@@ -23,3 +23,11 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		log.Println("encode error:", err)
 	}
 }
+
+func decodeJson(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	r.Body = http.MaxBytesReader(w, r.Body, 1<<20)
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	return decoder.Decode(dst)
+}
--- validate_chirpy.go
+++ validate_chirpy.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
+	"errors"
 	"net/http"
+	"strings"
 )
 
 func validateChirp(w http.ResponseWriter, r *http.Request) {
@@ -12,17 +12,43 @@ func validateChirp(w http.ResponseWriter, r *http.Request) {
 	}
 	params := parameters{}
 
-	err := json.NewDecoder(r.Body).Decode(&params)
-	if err != nil {
-		log.Println("decode error:", err)
+	if err := decodeJson(w, r, &params); err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
+
 	}
-	if len(params.Body) > 140 {
-		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+	cleaned_body, err := validateProfane(params.Body, []string{
+		"kerfuffle",
+		"sharbert",
+		"fornax",
+	})
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 	type validRes struct {
-		Valid bool `json:"valid"`
+		Cleaned_body string `json:"cleaned_body"`
 	}
-	respondWithJSON(w, http.StatusOK, validRes{Valid: true})
+	respondWithJSON(w, http.StatusOK, validRes{Cleaned_body: cleaned_body})
 
+}
+
+func validateProfane(content string, banWords []string) (string, error) {
+	if len(content) > 140 {
+		return "", errors.New("Chirp is too long")
+	}
+	mWord := make(map[string]bool)
+	for _, v := range banWords {
+		mWord[strings.ToLower(v)] = true
+	}
+
+	wContent := strings.Split(content, " ")
+	for i, w := range wContent {
+		_, ok := mWord[strings.ToLower(w)]
+		if ok {
+			wContent[i] = strings.Repeat("*", 4)
+		}
+	}
+	return strings.Join(wContent, " "), nil
 }
